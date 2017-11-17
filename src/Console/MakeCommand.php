4c008e9e0543701ed014e7d07ade010b471c91b5
Diego Lorenzo <diego@diegolorenzo.com>
2017-11-17 14:30:25 -0500

    Adding progress.

--- src/Console/MakeCommand.php
+++ src/Console/MakeCommand.php
@@ -15,7 +15,7 @@ class MakeCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'vulcan:make';
+    protected $signature = 'vulcan:make {resource?}';
 
     /**
      * The console command description.
@@ -23,6 +23,19 @@ class MakeCommand extends Command
      * @var string
      */
     protected $description = 'Scaffold CRUD files for a specified resource.';
+
+    protected $directories = [
+        'Http/Controllers/Admin' => 'controller',
+        'Http/Controllers/Client' => 'controller',
+    ];
+
+    // protected $controllers = [
+    //     //
+    // ];
+
+    // protected $views = [
+    //     //
+    // ];
 
     /**
      * Create a new command instance.
@@ -41,6 +54,16 @@ class MakeCommand extends Command
      */
     public function handle()
     {
+        $resource = $this->argument('resource') ?: $this->ask('What is the singular name of the resource you would like Vulcan to make?');
 
+        $this->info("Ok! Vulcan is forging a new \"{$resource}\" resource Migration, Model, Controllers, and Views.");
+
+        $stub = str_replace(
+            '{{namespace}}',
+            $this->getAppNamespace(),
+            file_get_contents(__DIR__.'/stubs/make/controllers/admin/pagecontroller.stub')
+        );
+
+        dd($stub);
     }
 }
